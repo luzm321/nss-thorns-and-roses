dbc5b95e5494373bedf21175da2a9a53ff25beb3
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-07-09 20:06:24 -0500

    initial commit; created components directory containing flowers, nav, and nurseries directory with corresponding provider, list, and individual item components; created ApplicationViews, ThornsAndRoses, and Home components, pending for data code to be added

---
+++ src/ThornsAndRoses.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavBar } from "./components/nav/NavBar.js";
+import { ApplicationViews } from "./ApplicationsViews.js";
+import "./ThornsAndRoses.css";
+
+
+export const ThornsAndRoses = () => (
+    <>
+      <NavBar />
+      <ApplicationViews />
+    </>
+);
